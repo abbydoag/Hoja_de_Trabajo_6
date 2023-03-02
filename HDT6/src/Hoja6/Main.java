b367a4d97c110ec0c4bba3c3f53431b37eff65bd
abbydoag <86386562+abbydoag@users.noreply.github.com>
2023-03-02 09:32:50 -0600

    Add files via upload

--- HDT6/src/Hoja6/Main.java
+++ HDT6/src/Hoja6/Main.java
@@ -1,5 +1,29 @@
 package Hoja6;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
-
+	private static String readProducts(String productFile) throws FileNotFoundException {
+		BufferedReader productScan=new BufferedReader(new FileReader(productFile));
+		String product="";
+		try { 
+			product=productScan.readLine();
+			
+			while (product!=null) {
+				System.out.println(product);
+				product=productScan.readLine();
+			}	
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(product);		
+		return product;
+	}
+	public static void main(String[]args) throws FileNotFoundException{
+		String inventory=readProducts("src/Hoja6/ListadoProducto.txt");
+		String selling=(inventory);
+		System.out.println("Productos: "+selling);
+		}
 }
